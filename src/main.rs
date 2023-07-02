37eb3f102bab567969d1dd46fe49c5f3275056d3
Andrew Moss <a.moss@sobolt.com>
2023-07-02 19:53:26 +0200

    part 1 complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+use aoc_2::part_1;
+
 fn main() {
-    println!("Hello, world!");
+    run_part_1();
+}
+
+fn run_part_1() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let output = part_1(&input);
+    println!("part 1: {}", output);
 }
